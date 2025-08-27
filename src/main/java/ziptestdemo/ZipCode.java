9ede2bf502ef7809cd2fc3a20a404ca8df977608
Astghik-hub <minastghik@gmail.com>
2025-08-26 23:15:51 -0400

    added comments

--- src/main/java/ziptestdemo/ZipCode.java
+++ src/main/java/ziptestdemo/ZipCode.java
@@ -6,88 +6,110 @@ package ziptestdemo;
 
 /**
  *
- * @author 2466920
+ * @author Astghik Minasyan 
+ * Lab_01: Review OOP 
+ * 25/08/2025
  */
 public class ZipCode {
-
     protected int Zip;
 
+    /**
+     * First constructor, encodes zip code as an integer
+     *
+     * @param num the number to store as the zip code
+     */
     public ZipCode(int num) {
+        // Pint an error message if the zip code is more than 5 digits
         if (num > 99999) {
-            System.out.printf("%d zip code is more than 5 digits %n", num);
+            System.out.printf("%d zip code is more than 5 digits%n", num);
         }
+        
         this.Zip = num;
     }
 
+    /**
+     * Second constructor, takes a bar code string consisting of 0’s and 1’s and
+     * encodes it as an integer
+     *
+     * @param barCode the input bar code string
+     */
     public ZipCode(String barCode) {
         boolean validCode = true;
-            if (barCode.length() != 27) {
-                System.out.println("Error: bar code must be in multiples of 5-binary digits");
-                this.Zip = 0;
-                return;
-            }
 
-            
-            if (barCode.charAt(0) != '1' || barCode.charAt(barCode.length() - 1) != '1') {
-                System.out.println("Error: bar code missing a 1 at start or end");
-                this.Zip = 0;
-                return;
-            }
+        // Check if the bar code is more than 27 digits
+        if (barCode.length() != 27) {
+            System.out.println("Error: bar code must be in multiples of 5-binary digits");
+            this.Zip = 0;
+            return;
+        }
 
-            for (int i = 1; i <= 25; i++) {
-                if (barCode.charAt(i) != '0' && barCode.charAt(i) != '1') {
-                    System.out.printf("bar code character: %c must be '0' or '1' %n", barCode.charAt(i));
+        // Check if the bar code starts and ends in '1'
+        if (barCode.charAt(0) != '1' || barCode.charAt(barCode.length() - 1) != '1') {
+            System.out.println("Error: bar code missing a 1 at start or end");
+            this.Zip = 0;
+            return;
+        }
+
+        // Check if the bar code contains an invalid digit
+        for (int i = 1; i <= 25; i++) {
+            if (barCode.charAt(i) != '0' && barCode.charAt(i) != '1') {
+                System.out.printf("bar code character: %c must be '0' or '1' %n", barCode.charAt(i));
+                validCode = false;
+            }
+        }
+
+        // Check if the bar code contains an invalide sequence
+        if (validCode) {
+            for (int i = 1; i <= 21; i += 5) {
+                String sequence = barCode.substring(i, i + 5);
+                int count1 = 0;
+                int count0 = 0;
+
+                for (int j = 0; j < 5; j++) {
+                    if (sequence.charAt(j) == '1') {
+                        count1++;
+                    } else {
+                        count0++;
+                    }
+                }
+                if (count1 != 2 || count0 != 3) {
+                    System.out.printf("%s has invalid sequence in the bar code%n", sequence);
                     validCode = false;
                 }
             }
-            
-            if (validCode) {
-                for (int i = 1; i <= 21; i += 5) {
-                    String sequence = barCode.substring(i, i + 5);
-                    int count1 = 0;
-                    int count0 = 0;
-                    
-                    for (int j = 0; j < 5; j++) {
-                        if (sequence.charAt(j) == '1') {
-                            count1++;
-                        } else {
-                            count0++;
-                        }
-                    }
-                    if (count1 != 2 || count0 != 3) {
-                        System.out.printf("%s has invalid sequence in the bar code %n", sequence);
-                        validCode = false;
-                    }
-                }
-            }
-            
-            if (validCode) {
-                this.Zip = ParseBarCode(barCode);
-            } else this.Zip = 0;
+        }
+
+        // If the bar code is valid, call ParseBarCode method
+        // to convert it and store it as an integer
+        if (validCode) {
+            this.Zip = ParseBarCode(barCode);
+        } else this.Zip = 0;
     }
 
     /**
-     * converts the ZIP code as a properly constructed string of binary digits
+     * Converts the zip code as a properly constructed string of binary digits
      *
-     * @return
+     * @return the string of binary digits corresponding to the zip code
      */
     public String GetBarCode() {
         String zipString = String.valueOf(Zip);
         String realZip = "";
         String barCode = "1";
 
+        // Remove the first digit if the zip code is more than 5 digits
         if (Zip > 99999) {
             realZip += zipString.substring(1);
         }
 
+        // Add leading 0's if the zip code is less than 5 digits
         if (Zip < 10000) {
             for (int i = 1; i <= 5 - zipString.length(); i++) {
                 realZip += "0";
             }
             realZip += zipString;
         } else realZip = zipString;
-        
 
+        // Construct the barcode string of 0's and 1's
         for (int i = 0; i < 5; i++) {
             barCode += switch (realZip.charAt(i)) {
                 case '0' -> "11000";
@@ -108,13 +130,15 @@ public class ZipCode {
     }
 
     /**
-     * converts the corresponding Zip code as an integer
+     * Decodes the binary bar code string
      *
-     * @param barCode
-     * @return Zip code as an integer
+     * @param barCode the input string to convert
+     * @return the bar code as an integer
      */
     private static int ParseBarCode(String barCode) {
         String value = "";
+
+        // Convert each 5-digit sequence into numbers
         for (int i = 1; i <= 21; i += 5) {
             value += switch (barCode.substring(i, i + 5)) {
                 case "11000" -> 0;
